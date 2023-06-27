a58b8f61e034832870eb8791960d7fa241e756e7
amcordobag@gmail.com <amcordobag@gmail.com>
2023-06-27 15:02:31 -0600

    Fix

--- cr_electronic_invoice/models/api_facturae.py
+++ cr_electronic_invoice/models/api_facturae.py
@@ -1030,6 +1030,7 @@ def load_xml_data(invoice, load_lines, account_id, product_id=False, analytic_ac
     invoice.date_issuance = invoice_xml.xpath("inv:FechaEmision", namespaces=namespaces)[0].text
     invoice.invoice_date = invoice.date_issuance
     invoice.tipo_documento = 'CCE'
+    invoice.state_invoice_partner = '1'
 
     emisor = invoice_xml.xpath("inv:Emisor/inv:Identificacion/inv:Numero", namespaces=namespaces)[0].text
     tipo_emisor = invoice_xml.xpath("inv:Emisor/inv:Identificacion/inv:Tipo", namespaces=namespaces)[0].text
