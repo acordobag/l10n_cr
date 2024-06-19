178020a88e718b55cf3b6b2ee1678e180fce9f38
amcordobag@gmail.com <amcordobag@gmail.com>
2024-06-19 01:00:11 -0600

    Fix computed

--- cr_electronic_invoice/models/account_move.py
+++ cr_electronic_invoice/models/account_move.py
@@ -172,22 +172,22 @@ class AccountInvoiceElectronic(models.Model):
     comp_amount_total = fields.Monetary(string='Total', readonly=True, compute='_compute_amount_total')
 
     def _compute_amount_total(self):
-        self.ensure_one()
-        currency = self.currency_id
-        currency_rate = 1
-        if not (currency.name == self.company_id.currency_id.name):
-            currency_rate = round(1.0 / currency.rate, 5)
-            
-        self.comp_amount_total = self.amount_total * currency_rate
+        for rec in self : 
+            currency = rec.currency_id
+            currency_rate = 1
+            if not (currency.name == rec.company_id.currency_id.name):
+                currency_rate = round(1.0 / currency.rate, 5)
+                
+            rec.comp_amount_total = rec.amount_total * currency_rate
         
     def _compute_amount_untaxed(self):
-        self.ensure_one()
-        currency = self.currency_id
-        currency_rate = 1
-        if not (currency.name == self.company_id.currency_id.name):
-            currency_rate = round(1.0 / currency.rate, 5)
-            
-        self.comp_amount_untaxed = self.amount_untaxed * currency_rate
+        for rec in self :
+            currency = rec.currency_id
+            currency_rate = 1
+            if not (currency.name == rec.company_id.currency_id.name):
+                currency_rate = round(1.0 / currency.rate, 5)
+                
+            rec.comp_amount_untaxed = rec.amount_untaxed * currency_rate
 
 
     def _compute_qr_code(self):
