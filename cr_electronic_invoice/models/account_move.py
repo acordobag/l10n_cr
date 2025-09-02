d9a15048eaee738cb6d1abe5518798c1c8eb982e
amcordobag@gmail.com <amcordobag@gmail.com>
2025-09-02 01:03:30 -0600

    Fix descuento comercial

--- cr_electronic_invoice/models/account_move.py
+++ cr_electronic_invoice/models/account_move.py
@@ -1205,6 +1205,7 @@ class AccountInvoiceElectronic(models.Model):
                             if inv_line.discount and price_unit > 0:
                                 total_descuento += descuento
                                 line["montoDescuento"] = descuento
+                                line["codigoDescuento"] = '01'
                                 line["naturalezaDescuento"] = inv_line.discount_note or 'Descuento Comercial'
 
                             # Se generan los impuestos
