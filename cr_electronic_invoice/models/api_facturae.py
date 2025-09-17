703204d150d8b7190924a846d5a40f400382c081
amcordobag@gmail.com <amcordobag@gmail.com>
2025-09-16 21:08:14 -0600

    Fix NC

--- cr_electronic_invoice/models/api_facturae.py
+++ cr_electronic_invoice/models/api_facturae.py
@@ -660,7 +660,7 @@ def gen_xml_v44(inv, sale_conditions, total_servicio_gravado,
     # Información de referencia
     if tipo_documento_referencia and numero_documento_referencia and fecha_emision_referencia:
         sb.append('<InformacionReferencia>')
-        sb.append('<'+nameTipoDoc+'>' + str(tipo_documento_referencia) + '<'+nameTipoDoc+'>')
+        sb.append('<'+nameTipoDoc+'>' + str(tipo_documento_referencia) + '</'+nameTipoDoc+'>')
         sb.append('<Numero>' + str(numero_documento_referencia) + '</Numero>')
         sb.append('<'+nameFechaEmision+'>' + fecha_emision_referencia + '</'+nameFechaEmision+'>')
         sb.append('<Codigo>' + str(codigo_referencia) + '</Codigo>')
