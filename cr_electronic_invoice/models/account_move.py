0a9a293a90320d0469ca4bee5fd0de1a66de6408
amcordobag@gmail.com <amcordobag@gmail.com>
2025-11-04 23:55:37 -0600

    Fix accpeting invoices

--- cr_electronic_invoice/models/account_move.py
+++ cr_electronic_invoice/models/account_move.py
@@ -1535,7 +1535,7 @@ class AccountInvoiceElectronic(models.Model):
                 inv.tipo_documento = 'disabled'
                 continue
 
-            self._onchange_partner_id(validate_payment=False)
+            # self._onchange_partner_id(validate_payment=False)
 
             if inv.partner_id.has_exoneration:
                 if inv.partner_id.date_expiration and (inv.partner_id.date_expiration > datetime.date.today()):
